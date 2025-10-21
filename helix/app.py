98cbee9b9f358ba0a54ae7f31d2cd00e22c86c01
Hennan Lewis <39320310+hennanlewis@users.noreply.github.com>
2025-10-21 11:13:47 -0300

    feat: create main Flask server structure and initial configuration
    - Implemented `create_app` function for Flask initialization
    - Registered `files_bp` and `api_bp` blueprints
    - Added base directory (`BASE_DIR`) and default port (8080)
    - Integrated `server.json` with server and theme options
    - Updated `main.py` to launch the app using `create_app()`

---
+++ helix/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from core.logging.logger import setup_logger
+from modules.files.routes import files_bp
+from api.routes import api_bp
+
+def create_app():
+    app = Flask(__name__, static_folder="static", template_folder="templates")
+
+    setup_logger(app)
+    app.config["BASE_DIR"] = "D:/Hennan"
+    # app.config["BASE_DIR"] = None
+
+    app.register_blueprint(files_bp)
+    app.register_blueprint(api_bp, url_prefix="/config")
+
+    return app
--- helix/main.py
+++ helix/main.py
@@ -1,6 +1,5 @@
-def main():
-    print("Hello from helix!")
-
+from app import create_app
 
 if __name__ == "__main__":
-    main()
+    app = create_app()
+    app.run(host="0.0.0.0", port=8080, debug=True)
